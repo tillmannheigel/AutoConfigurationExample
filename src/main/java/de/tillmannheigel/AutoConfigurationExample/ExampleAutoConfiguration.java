4823eec0a98ddf28b7552b46de7e1a498092caba
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-04-06 16:36:18 +0200

    another bean definition, now conditional.

--- src/main/java/de/tillmannheigel/AutoConfigurationExample/ExampleAutoConfiguration.java
+++ src/main/java/de/tillmannheigel/AutoConfigurationExample/ExampleAutoConfiguration.java
@@ -1,5 +1,6 @@
 package de.tillmannheigel.AutoConfigurationExample;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +10,12 @@ public class ExampleAutoConfiguration {
     @Bean
     public Integer integer() {
         return 5;
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(name = "answerToAllQuestions")
+    public Long answerToAllQuestions() {
+        return 42L;
     }
 }
