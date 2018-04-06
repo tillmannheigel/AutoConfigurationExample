965a71f88896668ea63cb62d0babdfca68e4ebff
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-04-06 16:05:00 +0200

    initial commit

---
+++ src/main/java/de/tillmannheigel/AutoConfigurationExample/AutoConfigurationExampleApplication.java
@@ -0,0 +1,12 @@
+package de.tillmannheigel.AutoConfigurationExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConfigurationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoConfigurationExampleApplication.class, args);
+	}
+}
